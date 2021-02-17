314e1591940514b74c8979c3b8495c47f8f8d94f
chrm <christof.mayer@gmail.com>
2021-02-17 18:19:03 +0100

    initial commit

---
+++ src/LetterOfApplication.js
@@ -0,0 +1,18 @@
+import './LetterOfApplication.css';
+
+function LetterOfApplication() {
+  return (
+    <div className="LetterOfApplication">
+        <div className="content">
+          <p>Dear Sirs,</p>
+          <p>My name is Christof Mayer and I would like to express my interest in applying for the position as a Frontend Developer.
+I’ve always dreamed of becoming a Software Developer and I believe that a position in the field of Frontend Development with React.js would set me miles ahead to reaching my goal.
+Please also note the documents I sent to office@goodbag.io.
+</p>
+<p>kind regards, Christof</p>
+        </div>
+    </div>
+  );
+}
+
+export default LetterOfApplication;
